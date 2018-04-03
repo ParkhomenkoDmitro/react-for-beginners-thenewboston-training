8a75de5f0b28282056eaaccad6b66b6803881604
Dmytro <dimagit24@gmail.com>
2018-04-03 18:53:52 +0300

    3-4 lesson

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
+import React from "react";
+import { render } from "react-dom";
 
-var createReactClass = require('create-react-class');
+var createReactClass = require("create-react-class");
 
 const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
+  fontFamily: "sans-serif",
+  textAlign: "center"
 };
 
-// const App = () => (
-//   <div style={styles}>
-//     <Hello name="CodeSandbox" />
-//     <h2>Start editing to see some magic happen {'\u2728'}</h2>
-//   </div>
-// );
 var Bacon = createReactClass({
   render: function() {
-    return (<h2>Test</h2>);
+    return (
+      <div>
+        <h2>Test</h2>
+        <p>Bla</p>
+      </div>
+    );
   }
 });
-render(<Bacon />, document.getElementById('root'));
+render(
+  <div>
+    <Bacon />
+    <Bacon />
+    <Bacon />
+  </div>,
+  document.getElementById("root")
+);
